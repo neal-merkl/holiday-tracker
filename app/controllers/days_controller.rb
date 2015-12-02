20ac1a3b25ba22af16d0c6f0899351ebdcf61865
Alfonso Merkl <anmerkl@gmail.com>
2015-12-02 16:10:15 -0500

    finished json portion

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,25 +1,24 @@
 class DaysController < ApplicationController
-  @@countries = ["BE", "BR", "CA", "CZ", "DE", "FR", "GB", "NO", "PL", "SK", "SL", "US"]
-  @@url = "http://holidayapi.com/v1/holidays?"
-
   def index
   end
 
   def show
+    @countries = ["BE", "BR", "CA", "CZ", "DE", "FR", "GB", "NO", "PL", "SK", "SL", "US"]
+    @url = "http://holidayapi.com/v1/holidays?"
+
     @day = params[:date]
+
     date = @day.split('-')
     day = date[0]
     month = date[1]
     year = date[2]
-    @strings = []
-    @holidays = []
-    @@countries.each do |c|
-      string = "#{@@url}country=#{c}&year=#{year}&month=#{month}&day=#{day}"
-      @strings << string
-      holiday = HTTParty.get("#{@@url}country=#{c}&year=#{year}&month=#{month}&day=#{day}")["holidays"][0]
-      @holidays << holiday
-    end
 
+    @holidays = []
+    @countries.each do |c|
+      HTTParty.get("#{@url}country=#{c}&year=#{year}&month=#{month}&day=#{day}")["holidays"].each do |h|
+        @holidays << h["name"]
+      end
+    end
   end
 
 end
