8892052acbc2e83a09f5c6f6fac641c978737561
Alfonso Merkl <anmerkl@gmail.com>
2015-12-02 18:01:19 -0500

    leaving date input for later

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,8 +3,8 @@ class DaysController < ApplicationController
   end
 
   def show
-    @countries = ["BE", "BR", "CA", "CZ", "DE", "FR", "GB", "NO", "PL", "SK", "SL", "US"]
-    @url = "http://holidayapi.com/v1/holidays?"
+    countries = ["BE", "BR", "CA", "CZ", "DE", "FR", "GB", "NO", "PL", "SK", "SL", "US"]
+    url = "http://holidayapi.com/v1/holidays?"
 
     @day = params[:date]
 
@@ -14,8 +14,8 @@ class DaysController < ApplicationController
     year = date[2]
 
     @holidays = []
-    @countries.each do |c|
-      HTTParty.get("#{@url}country=#{c}&year=#{year}&month=#{month}&day=#{day}")["holidays"].each do |h|
+    countries.each do |c|
+      HTTParty.get("#{url}country=#{c}&year=#{year}&month=#{month}&day=#{day}")["holidays"].each do |h|
         @holidays << h["name"]
       end
     end
