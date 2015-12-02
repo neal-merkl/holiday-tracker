da6aba4699d3b4ba0f8f97d81dfdfdf41ac4e956
Alfonso Merkl <anmerkl@gmail.com>
2015-12-02 14:03:31 -0500

    started functionality to retrieve holiday jsons for given days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,11 +1,20 @@
 class DaysController < ApplicationController
+  @@countries = ["BE", "BR", "CA", "CZ", "DE", "FR", "GB", "NO", "PL", "SK", "SL", "US"]
+  @@url = "http://holidayapi.com/v1/holidays?"
 
   def index
   end
 
   def show
     @day = params[:date]
-    @holiday = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+    date = @day.split('-')
+    day = date[0]
+    month = date[1]
+    year = date[2]
+    @holidays = []
+    @@countries.each do |c|
+      @holidays << HTTParty.get("#{@@url}country=#{c}&year=#{year}&month=#{month}&day=#{day}")
+    end
   end
 
 end
