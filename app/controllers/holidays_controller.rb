1825e2909d669bced9850f9325cff4e27c442285
Alfonso Merkl <anmerkl@gmail.com>
2015-12-04 16:28:14 -0500

    finished json parsing and holiday info views

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,15 +1,15 @@
 class HolidaysController < ApplicationController
 
   def show
-    holiday = params[:holiday]
-    country = params[:country]
-    year = params[:year]
+    @holiday = params[:holiday]
+    @country = params[:country]
+    @year = params[:year]
 
-    @country_occurences = get_year_holidays(year)
-    @country_occurences.select! { |i| i["name"].eql? holiday }
+    @country_occurences = get_year_holidays(@year)
+    @country_occurences.select! { |i| i["name"].eql? @holiday }
 
-    @year_occurrences = get_country_holidays(country, year.to_i)
-    @year_occurrences.select! { |d, n| n.eql? holiday }
+    @year_occurrences = get_country_holidays(@country, @year.to_i)
+    @year_occurrences.select! { |d, n| n.eql? @holiday }
   end
 
 end
