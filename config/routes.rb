9de91f6ee31e8526e1c7702b2ea9c2dc18ecc4ec
Alfonso Merkl <anmerkl@gmail.com>
2015-12-07 11:37:22 -0500

    added routes for holidays

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'days#index'
+  root            'days#index'
   get '/:date' => 'days#show', as: 'date'
+
   get '/:holiday/:country/:year' => 'holidays#show'
+
+  get '/holidays/new'            => 'holidays#new'
+  post '/holidays/'              => 'holidays#create'
+  get 'holidays/:id'             => 'holidays#edit'
+  patch 'holidays/:id'           => 'holidays#update'
+  delete 'holidays/:id'          => 'holidays#destroy'
 
 end
