a675b74f849f64a192b466c6811c9e39893aeba8
Alfonso Merkl <anmerkl@gmail.com>
2015-12-09 15:09:31 -0500

    updated holiday show view

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -10,7 +10,7 @@ class HolidaysController < ApplicationController
     @year = params[:year]
 
     @country_occurences = get_year_holidays(@year)
-    @country_occurences.select! { |i| i["name"].include? @holiday }
+    @country_occurences.select! { |i| i["name"].eql? @holiday }
 
     @year_occurrences = get_country_holidays(@country, @year.to_i)
     @year_occurrences.select! { |d, n| n.eql? @holiday }
